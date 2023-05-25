274aa5fd0b2288d92c824ad0f3acb35ce1301bc5
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-26 02:04:16 +0300

    feat : add deleting

--- src/main/java/org/gassion/SpringApp/dao/PersonDAO.java
+++ src/main/java/org/gassion/SpringApp/dao/PersonDAO.java
@@ -38,4 +38,8 @@ public class PersonDAO {
         Person personToBeUpdated = show(id);
         personToBeUpdated.setName(person.getName());
     }
+
+    public void delete(int id) {
+        people.removeIf(person -> person.getId() == id);
+    }
 }
