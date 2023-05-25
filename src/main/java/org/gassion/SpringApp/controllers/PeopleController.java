6594570f0895bd63b86325770afa15affb4e5832
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-25 22:45:36 +0300

    feat : add controllers

--- src/main/java/org/gassion/SpringApp/controllers/PeopleController.java
+++ src/main/java/org/gassion/SpringApp/controllers/PeopleController.java
@@ -1,12 +1,11 @@
 package org.gassion.SpringApp.controllers;
 
 import org.gassion.SpringApp.dao.PersonDAO;
+import org.gassion.SpringApp.models.Person;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("people")
@@ -30,5 +29,19 @@ public class PeopleController {
         model.addAttribute("people", personDAO.show(id));
 
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model) {
+        model.addAttribute("person", new Person());
+
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+
+        return "redirect/people";
     }
 }
