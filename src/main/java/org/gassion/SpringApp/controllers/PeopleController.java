bde45b11401e83084ffdebae89864f0964ecf239
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-26 01:49:17 +0300

    feat : add edit

--- src/main/java/org/gassion/SpringApp/controllers/PeopleController.java
+++ src/main/java/org/gassion/SpringApp/controllers/PeopleController.java
@@ -47,4 +47,10 @@ public class PeopleController {
         return "people/edit";
     }
 
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("person") Person person, @PathVariable("id") int id) {
+        personDAO.update(id, person);
+        return "redirect:/people";
+    }
+
 }
