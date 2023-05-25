506639cc94899ab91d8df196cb2f63fad30130f4
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-25 21:10:33 +0300

    feat : add controller

---
+++ src/main/java/org/gassion/SpringApp/controllers/PeopleController.java
@@ -0,0 +1,22 @@
+package org.gassion.SpringApp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("people")
+public class PeopleController {
+
+    @GetMapping()
+    public String index(Model model) {
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        return null;
+    }
+}
