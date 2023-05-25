2f8472f1f5d073ba082dda357850f166814ee6ed
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-26 01:23:39 +0300

    feat : add people edit controller

--- src/main/java/org/gassion/SpringApp/controllers/PeopleController.java
+++ src/main/java/org/gassion/SpringApp/controllers/PeopleController.java
@@ -20,28 +20,31 @@ public class PeopleController {
     @GetMapping()
     public String index(Model model) {
         model.addAttribute("people", personDAO.index());
-
         return "people/index";
     }
 
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("people", personDAO.show(id));
-
         return "people/show";
     }
 
     @GetMapping("/new")
     public String newPerson(Model model) {
         model.addAttribute("person", new Person());
-
         return "people/new";
     }
 
     @PostMapping()
     public String create(@ModelAttribute("person") Person person) {
         personDAO.save(person);
-
         return "redirect:/people";
     }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id){
+        model.addAttribute("person", personDAO.show(id));
+        return "people/edit";
+    }
+
 }
