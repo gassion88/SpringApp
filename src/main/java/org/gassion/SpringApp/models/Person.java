7c02ad0f21d3c58d19463bbe3abb549be24cac4d
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-26 11:21:43 +0300

    refactor :

--- src/main/java/org/gassion/SpringApp/models/Person.java
+++ src/main/java/org/gassion/SpringApp/models/Person.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
-import org.hibernate.validator.constraints.Range;
 
 public class Person {
     private int id;
