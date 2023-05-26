8c10ac6b35a029cb9d5f7133f2d8aaf261cbc8e1
Soslan Gassiev <soslangassity04@gmail.com>
2023-05-26 10:45:03 +0300

    feat : add validation to fields age, and email

--- src/main/java/org/gassion/SpringApp/models/Person.java
+++ src/main/java/org/gassion/SpringApp/models/Person.java
@@ -1,9 +1,23 @@
 package org.gassion.SpringApp.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import org.hibernate.validator.constraints.Range;
+
 public class Person {
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
+
+    @Min(value = 0, message = "Age should be greater than 0")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
 
